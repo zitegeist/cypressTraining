668098c071715a0f76ae04f3186b78e2a07b1cd4
kelly-anne o'connell <kelly-anne.oconnell@inspiretec.com>
2026-07-07 07:35:39 +0100

    res commit 1

---
+++ cypress/e2e/Kelly4.cy.ts
@@ -0,0 +1,15 @@
+describe('Arrivals and Departures Report', () => {
+    it('should generate the report successfully', () => {
+        cy.visit('https://dev-tlcore34.internal.inspiretec.com/Travelink/ui/#/')
+            cy.get('#add1Database').select('Q244_MLQA_CAMC_11Feb26_Automation_Testing') 
+            cy.get('txtUserName').type('kelly-anne.oconnell')
+            cy.get('#txtPassword').type('Comtec123!')
+            cy.get('btnlogin').click()
+
+            cy.contains('Reports').click()
+            cy.contains('Arrivals and Departures Report').click()
+            cy.url().should('include', '/Travelink/ui/#/reports/arrivals-departures')
+
+            cy.contains('Arrivals and Departures Report').should('be.visible')
+  })
+})
