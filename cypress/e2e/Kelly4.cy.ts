7e50076c79eac5c20465e355846d1ab934fab5df
kelly-anne o'connell <kelly-anne.oconnell@inspiretec.com>
2026-07-07 09:13:27 +0100

    commit 15

--- cypress/e2e/Kelly4.cy.ts
+++ cypress/e2e/Kelly4.cy.ts
@@ -9,6 +9,6 @@ describe('Arrivals and Departures Report', () => {
         cy.contains('Arrivals and Departures Report').click();
         cy.url().should('include', '/Travelink/ui/#/reports/arrivals-and-departures-report');
 
-        cy.contains('Subtitle:search arrivals and departures').should('be.visible');
+        cy.contains('Sarch arrivals and departures').should('be.visible');
   })
 })
