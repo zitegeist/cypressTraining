426c2334dbd96b352b091e15cd9d5e1e3d73e4b8
kelly-anne o'connell <kelly-anne.oconnell@inspiretec.com>
2026-07-07 08:31:31 +0100

    commit 7

--- cypress/e2e/Kelly4.cy.ts
+++ cypress/e2e/Kelly4.cy.ts
@@ -1,15 +1,15 @@
 describe('Arrivals and Departures Report', () => {
     it('should generate the report successfully', () => {
-        cy.visit('https://dev-tlcore34.internal.inspiretec.com/Travelink/ui/#/')
-            cy.get('#addDatabase').select('Q244_MLQA_CAMC_11Feb26_Automation_Only'
-            cy.get('txtUserName').type('kelly-anne.oconnell')
-            cy.get('#txtPassword').type('Comtec123!')
-            cy.get('btnlogin').click()
+        cy.visit('https://dev-tlcore34.internal.inspiretec.com/Travelink/ui/#/');
+        cy.get('#addDatabase').select('Q244_MLQA_CAMC_11Feb26_Automation_Only');
+        cy.get('#txtUserName').type('kelly-anne.oconnell');
+        cy.get('#txtPassword').type('Comtec123!');
+        cy.get('#btnlogin').click();
 
-            cy.contains('Reports').click()
-            cy.contains('Arrivals and Departures Report').click()
-            cy.url().should('include', '/Travelink/ui/#/reports/arrivals-departures')
+        cy.contains('Reports').click();
+        cy.contains('Arrivals and Departures Report').click();
+        cy.url().should('include', '/Travelink/ui/#/reports/arrivals-departures');
 
-            cy.contains('Arrivals and Departures Report').should('be.visible')
+        cy.contains('Arrivals and Departures Report').should('be.visible');
   })
 })
