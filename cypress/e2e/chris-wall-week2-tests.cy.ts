285999ad8131ca07db611c24d07cb9396ea8c711
Chris Wall <Chris.Wall@inspiretec.com>
2026-07-07 08:51:30 +0100

    Add week 2 tests

--- cypress/e2e/chris-wall-week2-tests.cy.ts
+++ cypress/e2e/chris-wall-week2-tests.cy.ts
@@ -1,28 +1,41 @@
 describe('Automation Login Tests', () => {
-  it('Navigate to Login Page and succesfully login', () => {
-    cy.visit ('/login') 
-    cy.url().should('include', '/login')
-    cy.get('[data-qa="login-email"]').type('chris.wall@inspiretec.com')
-    cy.get('[data-qa="login-password"]').type('Comtec123!')
-    cy.get('[data-qa="login-button"]').click()
-    //Verify Redirected to home page on login
-    cy.url().should('include', '/')
-    //Verify 'logged in as.....' text is visible
-    cy.contains('Logged in as Chris Wall').should('be.visible')
-    //Verify logout button is visible
-    cy.contains('Logout').should('be.visible')
+  describe('Valid login details', () => {
+    beforeEach(() => {
+      //Navigate to Login Page before each test
+      cy.visit('/login')
+      //Verify URL contains /login
+      cy.url().should('include', '/login')
+      //Enter valid email and password and click login button
+      cy.get('[data-qa="login-email"]').type('chris.wall@inspiretec.com')
+      cy.get('[data-qa="login-password"]').type('Comtec123!')
+      cy.get('[data-qa="login-button"]').click()
+    })
+
+    it('Login succesfully', () => {
+      //Verify Redirected to home page on login
+      cy.url().should('include', '/')
+      //Verify 'logged in as.....' text is visible
+      cy.contains('Logged in as Chris Wall').should('be.visible')
+      //Verify logout button is visible
+      cy.contains('Logout').should('be.visible')
+    })
+
+    it('Logout succesfully', () => {
+      //Click logout button
+      cy.get('a[href="/logout"]').click()
+      //Verify Redirected to home page on login
+      cy.url().should('include', '/login')
+      //Verify Signup/Login button is visible
+      cy.contains('Signup / Login').should('be.visible')
+    })
   })
 
-   //it('Logout and verify', () => {
-    //cy.get( 'a[href="/logout"]').click()
-    //Verify Redirected to home page on login
-    //cy.url().should('include', '/login')
-    //Verify Signup/Login button is visible
-    //cy.contains('a[href="/login"]').should('be.visible')
-  //})
-
-  it('Failed login and verify', () => {
+  it('Failed login', () => {
+    //Navigate to Login Page before each test
     cy.visit('/login')
+    //Verify URL contains /login
+    cy.url().should('include', '/login')
+    //Enter invalid email and password and click login button
     cy.get('[data-qa="login-email"]').type('invalid@email.com')
     cy.get('[data-qa="login-password"]').type('invalidpassword')
     cy.get('[data-qa="login-button"]').click()
