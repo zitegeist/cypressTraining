71e8b5b4f57105f653d531a99b565719ce55fbc5
nichola matthews <nichola.matthews@inspiretec.com>
2026-07-13 10:50:43 +0100

    open report screen changed url and db

--- cypress/e2e/newfilenicm13jul.cy.ts
+++ cypress/e2e/newfilenicm13jul.cy.ts
@@ -1,8 +1,8 @@
 describe('Travelink Navigation', () => {
   it('should open the Reports page', () => {
     // Login first
-    cy.visit('https://dev-tlcore34.internal.inspiretec.com/Travelink/ui/#/')
-    cy.get('#ddlDatabase').select('Q244_MLQA_CAMC_11Feb26_Automation_Only')
+    cy.visit('https://dev-tlcore11.internal.inspiretec.com/Travelink/ui/#/')
+    cy.get('#ddlDatabase').select('Q244_MLQA_COOP_13Oct25')
     cy.get('#txtUserName').type('nichola.matthews')
     cy.get('#txtPassword').type('Comtec123!')
     cy.get('#btnLogin').click()
