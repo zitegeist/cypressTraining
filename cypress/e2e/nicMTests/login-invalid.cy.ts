7cf61c7d35019b7e4b8a766607b7e763e6b87043
nichola matthews <nichola.matthews@inspiretec.com>
2026-07-23 13:43:53 +0100

    week5homeworknicm

--- cypress/e2e/nicMTests/login-invalid.cy.ts
+++ cypress/e2e/nicMTests/login-invalid.cy.ts
@@ -1,14 +1,14 @@
 describe('Login Tests', () => {
-  it('Login - Successful', () => {
+  it('Login - successful', () => {
     cy.fixture('users').then((users) => {
       cy.login(users.validUser.email, users.validUser.password)
       cy.contains('Logout').should('be.visible')
     })
   })
-  it('Login - Successful', () => {
+  it('Login - unsuccessful', () => {
     cy.fixture('users').then((users) => {
-      cy.login(users.validUser.email, users.validUser.password)
-      cy.contains('Logout').should('be.visible')
+      cy.login(users.invalidUser.email, users.invalidUser.password)
+      cy.contains('Login').should('be.visible')
     })
   })
 })
